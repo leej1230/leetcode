589e8a1dc685d8ab875cc390825a6425c7ca277e
leej1230 <leejaewoo1230@gmail.com>
2022-10-11 23:35:27 -0700

    20221011

---
+++ MergeIntervals.py
@@ -0,0 +1,11 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda x:x[0])
+        ans = []
+        for interval in intervals:
+            if not ans or ans[-1][1] < interval[0]:
+                ans.append(interval)
+            else:
+                ans[-1][1] = max(interval[1],ans[-1][1])
+        
+        return ans
