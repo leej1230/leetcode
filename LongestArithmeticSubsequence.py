0b85e629e89bb273b4cd5258fe144d38963c2542
leej1230 <leejaewoo1230@gmail.com>
2022-10-24 00:57:34 -0700

    20221023

---
+++ LongestArithmeticSubsequence.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestArithSeqLength(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [{} for _ in range(n)]
+        ans = 0
+        for i in range(n):
+            for j in range(i):
+                diff = nums[i] - nums[j]
+                if diff not in dp[j]:
+                    dp[i][diff] = 2
+                else:
+                    dp[i][diff] = dp[j][diff] + 1
+                ans = max(ans, dp[i][diff])
+        return ans
